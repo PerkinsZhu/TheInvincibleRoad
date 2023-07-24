fd58905bc4bdbcf595bf5065df9eb7dc65c6c570
PerkinsZhu <perkinszhu@sina.cn>
2023-07-24 09:51:02 +0800

    NIO

---
+++ JavaBase/src/main/java/com/perkins/base/NioApp.java
@@ -0,0 +1,12 @@
+package com.perkins.base;
+
+/**
+ * @author: perkins Zhu
+ * @date: 2023/7/24 9:18
+ * @description:
+ **/
+public class NioApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
